f4a524a2e22df5bc69c344dfe5ee3f3f3696c00f
Jonathan Fortunati <Jonathan.Fortunati@smartbear.com>
2018-07-25 14:29:07 -0400

    added more debug messages to test.bat

--- test.bat
+++ test.bat
@@ -1,7 +1,12 @@
-ECHO 'starting server...' >> output.txt
+ECHO starting test script... >> output.txt
 cd ./application-server
+ECHO successfully changed directory > output.txt
+ECHO starting server... > output.txt
 npm start
-ECHO 'starting front end...' >> output.txt
+ECHO started server! > output.txt
 cd ..
-cd ./web-server
+cd ./web-server/memory
+ECHO successfully changed directory > output.txt
+ECHO starting front end... > output.txt
 npm start
+ECHO started front end! > output.txt
