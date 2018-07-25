5773fdba3c84e80129b9fd50fad0b20783b9355c
Jonathan Fortunati <Jonathan.Fortunati@smartbear.com>
2018-07-25 14:15:09 -0400

    modified test.bat to start web and server

--- test.bat
+++ test.bat
@@ -1,1 +1,7 @@
-ECHO "Made by Jenkins" >> test.txt
+ECHO 'starting server...' >> output.txt
+cd ./application-server
+npm start
+ECHO 'starting front end...' >> output.txt
+cd ..
+cd ./web-server
+npm start
