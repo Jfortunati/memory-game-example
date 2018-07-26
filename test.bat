6bac6854e0dfd2305b01f5384d68302916fc88e0
Jonathan Fortunati <Jonathan.Fortunati@smartbear.com>
2018-07-26 18:21:43 -0400

    modified how I change directory

--- test.bat
+++ test.bat
@@ -1,12 +1,12 @@
 ECHO starting >> output.txt
 ECHO starting test script... >> output.txt
-cd ./application-server
+cd application-server
 ECHO successfully changed directory > output.txt
 ECHO starting server... > output.txt
 npm start
 ECHO started server! > output.txt
 cd ..
-cd ./web-server/memory
+cd web-server\memory
 ECHO successfully changed directory > output.txt
 ECHO starting front end... > output.txt
 npm start
