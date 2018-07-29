53b1ea8f360e119b758666dcc2f4f7bd25a42978
Jonathan Fortunati <Jonathan.Fortunati@smartbear.com>
2018-07-29 15:25:06 -0400

    call npm start

--- test.bat
+++ test.bat
@@ -2,11 +2,11 @@ ECHO starting test script... >> output.txt
 cd application-server
 ECHO successfully changed directory > output.txt
 ECHO starting server... > output.txt
-npm start
+call npm start
 ECHO started server! > output.txt
 cd ..
 cd web-server\memory
 ECHO successfully changed directory > output.txt
 ECHO starting front end... > output.txt
-npm start
+call npm start
 ECHO started front end! > output.txt
