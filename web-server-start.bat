4377c00f33bd7d475aaee6f0e0fc1d41469b2fb5
Jonathan Fortunati <Jonathan.Fortunati@smartbear.com>
2018-09-17 15:54:19 -0400

    huh?

--- web-server-start.bat
+++ web-server-start.bat
@@ -2,3 +2,4 @@ cd web-server\memory
 ECHO successfully changed directory > output.txt
 ECHO starting front end... > output.txt
 CALL npm start
+CALL ccollab review create --titel "my new review!" --files
